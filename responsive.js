ce602a4211b5817948af87fbded6419288fa09b1
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-03-19 09:14:28 +0100

    .

--- responsive.js
+++ responsive.js
@@ -6,3 +6,17 @@ function hide(){
 	document.getElementById('myDropdown').style.display='none';
 	document.getElementById('dropdownbtn').onclick=show;
 }
+
+
+
+
+
+function show2() {
+	document.getElementById('myDropdown').style.display='block';
+	document.getElementById('dropdownbtn2').onclick=hide2;
+	document.getElementById('myDropdown').style.top='500px';
+} 
+function hide2(){
+	document.getElementById('myDropdown').style.display='none';
+	document.getElementById('dropdownbtn2').onclick=show2;
+}
